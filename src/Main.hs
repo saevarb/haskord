51e8c75b24a164ca20d9c0adf7012454a436e6e1
Sævar Berg <icedane@gmail.com>
2018-02-18 18:07:16 +0100

    Hlinted stuff

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE PatternSynonyms #-}
@@ -14,10 +13,9 @@ import Control.Concurrent
 import Control.Concurrent.MVar
 
 import Data.ByteString (ByteString)
-import Network.WebSockets
 import Data.Aeson
 import Network.HTTP.Req
-import Network.WebSockets (ClientApp, receiveData, sendClose, sendTextData)
+import Network.WebSockets (Connection, ClientApp, receiveData, sendClose, sendTextData)
 import Wuss
 
 
@@ -34,17 +32,17 @@ instance MonadHttp IO where
     handleHttpException = throwIO
 
 dispatch :: MVar Int -> Connection -> GatewayMessage Payload -> IO ()
-dispatch seqVar conn (d -> Just (HeartbeatPayload {..})) = do
+dispatch seqVar conn (d -> Just HeartbeatPayload {..}) =
     void $ forkIO $ do
-        threadDelay (heartbeatInterval * 100)
+        threadDelay (heartbeatInterval * 1000)
         seqNo <- tryReadMVar seqVar
         sendTextData conn (mkHeartbeat seqNo)
-dispatch _     _ _ = do
+dispatch _     _ _ =
     return ()
 
-updateSeqNo :: MVar Int -> (Maybe Int) -> IO ()
+updateSeqNo :: MVar Int -> Maybe Int -> IO ()
 updateSeqNo _ Nothing = return ()
-updateSeqNo var (Just s) = do
+updateSeqNo var (Just s) =
     putMVar var s
 
 app :: Connection -> IO b
@@ -67,7 +65,7 @@ main :: IO ()
 main = do
     let (Just (parsedUrl, _)) = parseUrlHttps apiEndpoint
         opt = header "Authorization" ("Bot " <> botToken) <>
-              header "User-Agent" ("DiscordBot (https://github.com/saevarb/haskord, 0.1)")
+              header "User-Agent" "DiscordBot (https://github.com/saevarb/haskord, 0.1)"
     res <- req GET (parsedUrl /: "gateway" /: "bot") NoReqBody jsonResponse opt
     let gwResponse = responseBody res :: GatewayResponse
     print gwResponse

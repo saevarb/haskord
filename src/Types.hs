a69a28f8ebe05137d90060dbf29c10f0716034c2
Sævar Berg <icedane@gmail.com>
2018-02-18 21:47:12 +0100

    Added EventType

--- src/Types.hs
+++ src/Types.hs
@@ -59,12 +59,54 @@ decodingOptions =
     , omitNothingFields = True
     }
 
+data EventType
+    = HELLO
+    | READY
+    | RESUMED
+    | INVALID_SESSION
+    | CHANNEL_CREATE
+    | CHANNEL_UPDATE
+    | CHANNEL_DELETE
+    | CHANNEL_PINS_UPDATE
+    | GUILD_CREATE
+    | GUILD_UPDATE
+    | GUILD_DELETE
+    | GUILD_BAN_ADD
+    | GUILD_BAN_REMOVE
+    | GUILD_EMOJIS_UPDATE
+    | GUILD_INTEGRATIONS_UPDATE
+    | GUILD_MEMBER_ADD
+    | GUILD_MEMBER_REMOVE
+    | GUILD_MEMBER_UPDATE
+    | GUILD_MEMBERS_CHUNK
+    | GUILD_ROLE_CREATE
+    | GUILD_ROLE_UPDATE
+    | GUILD_ROLE_DELETE
+    | MESSAGE_CREATE
+    | MESSAGE_UPDATE
+    | MESSAGE_DELETE
+    | MESSAGE_DELETE_BULK
+    | MESSAGE_REACTION_ADD
+    | MESSAGE_REACTION_REMOVE
+    | MESSAGE_REACTION_REMOVE_ALL
+    | PRESENCE_UPDATE
+    | TYPING_START
+    | USER_UPDATE
+    | VOICE_STATE_UPDATE
+    | VOICE_SERVER_UPDATE
+    | WEBHOOKS_UPDATE
+    deriving (Show, Eq, Enum, Generic)
+
+instance FromJSON EventType
+instance ToJSON EventType
+
+
 data GatewayMessage payloadType
     = GatewayMessage
     { op :: GatewayOpcode
     , d  :: Maybe payloadType
     , s  :: Maybe Int
-    , t  :: Maybe Text
+    , t  :: Maybe EventType
     } deriving (Generic, Show)
 
 instance (ToJSON a) => ToJSON (GatewayMessage a)
